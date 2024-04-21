ce5f7d39b162c375fc35a53d321f5e33d4a6cfcb
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-04-21 14:07:52 +0400

    update typings

--- dist/src/types/notification/constants.js
+++ dist/src/types/notification/constants.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NOTIFICATION_STATUS_LIST = exports.NOTIFICATION_LIST = void 0;
 exports.NOTIFICATION_LIST = [
-    'PENDING_FRIEND_REQUEST',
+    'NEW_FRIEND_REQUEST',
     'ACCEPTED_FRIEND_REQUEST',
     'MESSAGE',
     'POST',
